c7af27ed0097597d282cfe71cfa72e8f05fb7efb
marcelordrgs98@gmail.com <marcelordrgs98@gmail.com>
2019-11-19 18:00:55 -0300

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # busyIndicator
 Busy indicator to use in web sites and web apps
+
+
+# How to use
+To use this in your app is to easy, you only need download script and add him in tag <head> of your html file.
+* <script src="js/busyIndicator.js"></script>
+
+After this you are able to call him with methods as busyIndicator.open(); to show busy or busyIndicator.hide(); to don't show anymore.
---
+++ src/busyIndicator.js
@@ -0,0 +1,78 @@
+/* DESENVOLVIDO POR: MARCELO MONTALVAO */
+// github.com/marcelo-rm
+
+// METHODS
+function open(){
+    var busy = document.getElementById("busy");
+    if(busy){
+        busy.style.display = 'flex';
+    } else {
+        document.getElementsByTagName("body")[0].append(busyContainer);
+    }
+}
+
+function close(){
+    document.getElementById("busy").style.display = "none";
+}
+
+//STYLES
+var style = document.createElement('style');
+style.type = 'text/css';
+var keyFrames = '\
+@-webkit-keyframes busy {\
+    100% {\
+        transform: rotateZ(360deg);\
+    }\
+}\
+@-moz-keyframes busy {\
+    100% {\
+        transform: rotateZ(360deg);\
+    }\
+}';
+style.innerHTML = keyFrames;
+document.getElementsByTagName('head')[0].appendChild(style);
+
+var busyStyle = `
+    z-index: 1000;
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    align-items: center;
+    position: fixed;
+    left: 0px;
+    top: 0px;
+    margin: 0px;
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(0, 0, 0, 0.2);
+    font-size: 5rem;
+    color: blue;
+`;
+var iconStyle = `
+    width: 100px;
+    height: 100px;
+    animation: busy 1.5s linear infinite;
+    margin: 0;
+    border: 6px solid #1d262d;
+    border-right-color: transparent;
+    border-radius: 100%;
+    vertical-align: middle;
+`;
+
+//<i class="fas fa-circle-notch"></i>
+//HTML OBJECTS
+var icon = document.createElement("i");
+icon.class = "fas fa-circle-nortch";
+icon.style = iconStyle;
+
+var busyContainer = document.createElement("div");
+busyContainer.appendChild(icon);
+
+busyContainer.id = "busy";
+busyContainer.style = busyStyle;
+
+//MASTER OBJECT
+var busyIndicator = {
+    open: open,
+    close: close
+}
