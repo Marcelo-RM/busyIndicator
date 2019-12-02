d3384c62be4613b12624b30dbf7d7dbe6c53617f
Marcelo-RM <marcelordrgs98@gmail.com>
2019-12-02 14:41:12 -0200

    new type busy

--- README.md
+++ README.md
@@ -8,5 +8,17 @@ To use this in your app is to easy, you only need download script and add him in
 
 After this you are able to call him with methods as busyIndicator.open(); to show busy or busyIndicator.close(); to don't show anymore.
 
-You can use time delay and colors with params
-busyIndicator.open('2s', 'blue');
+You can use time delay in seconds
+busyIndicator.open('2s');
+
+you can use colors in busy indicator
+busyIndicator.open('2s', 'red');
+busyIndicator.open('2s', '#ff0000');
+busyIndicator.open('2s', 'rgb(255, 0, 0)');
+
+choose from two indicator options, circle or bubbles
+if the third parameter was not passed the indicator will be the default, circle
+
+busyIndicator.open('2s', 'red');             //circle busy
+busyIndicator.open('2s', 'red', 'busy');     //circle busy
+busyIndicator.open('2s', 'red', 'bubble');   //bubble busy
--- src/busyIndicator.js
+++ src/busyIndicator.js
@@ -6,23 +6,42 @@
  * 
  * @param {Number} delay seconds to wait
  */
-function open(delay, color){
+function open(delay, color, type){
+    var type = type ? type : "busy";
     var time = delay ? _convetTime(delay) : 0;
-    setTimeout(() => {
-        var busy = document.getElementById("busy");
-        if(busy){
-            busy.style.display = 'flex';
-        } else {
-            document.getElementsByTagName("body")[0].append(busyContainer);
-        }
 
-        busyContainer.children[0].style.borderColor = color;
-        busyContainer.children[0].style.borderRightColor = 'transparent';
+    setTimeout(() => {
+        var busy = document.getElementById(type);
+        
+        if(type == "bubble"){
+            if(busy){
+                busy.style.display = 'flex';
+            } else {
+                document.getElementsByTagName("body")[0].append(busyContainerBubble);
+                //document.getElementsByTagName("body")[0].append(busyContainer);
+            }
+            document.getElementsByClassName("bubbles01")[0].style.backgroundColor = color;
+            document.getElementsByClassName("bubbles02")[0].style.backgroundColor = color;
+            document.getElementsByClassName("bubbles03")[0].style.backgroundColor = color;
+        } else {
+            if(busy){
+                busy.style.display = 'flex';
+            } else {
+                document.getElementsByTagName("body")[0].append(busyContainer);
+            }
+            busyContainer.children[0].style.borderColor = color;
+            busyContainer.children[0].style.borderRightColor = 'transparent';
+        }
     }, time);
 }
 
 function close(){
-    document.getElementById("busy").style.display = "none";
+    var itens = document.getElementsByClassName("busy");
+    itens = Array.from(itens);
+
+    itens.forEach(e => {
+        e.style.display = "none";
+    });
 }
 
 function _convetTime(nTime){
@@ -38,10 +57,27 @@ var keyFrames = '\
     100% {\
         transform: rotateZ(360deg);\
     }\
-}\
+} \
 @-moz-keyframes busy {\
     100% {\
         transform: rotateZ(360deg);\
+    }\
+} \
+@keyframes pop {\
+    0% {\
+        width: 10px;\
+        height: 10px;\
+        opacity: 1; \
+    }\
+    50% {\
+        width: 25px;\
+        height: 25px;\
+        opacity: 0.3;\
+    }\
+    100% {\
+        width: 10px;\
+        height: 10px;\
+        opacity: 1;\
     }\
 }';
 style.innerHTML = keyFrames;
@@ -64,8 +100,8 @@ var busyStyle = `
     color: blue;
 `;
 var iconStyle = `
-    width: 100px;
-    height: 100px;
+    width: 80px;
+    height: 80px;
     animation: busy 1.5s linear infinite;
     margin: 0;
     border: 6px solid #1d262d;
@@ -73,6 +109,60 @@ var iconStyle = `
     border-radius: 100%;
     vertical-align: middle;
 `;
+
+//  STYLES PARA BUSY DE CIRCULOS
+var busyRoundedStyle = "\
+.busy { \
+    z-index: 1000; \
+    position: fixed; \
+    left: 0px; \
+    top: 0px; \
+    margin: 0px; \
+    width: 100vw; \
+    height: 100vh; \
+    background-color: rgba(0, 0, 0, 0.2);\
+    font-size: 5rem;\
+    display: flex;\
+    justify-content: center;\
+    align-items: center;\
+}\
+.bubbles01 {\
+    background-color: rgb(76, 0, 130);\
+    margin-left: -30px;\
+    position: absolute;\
+    border-radius: 50%;\
+    animation-name: pop;\
+    animation-duration: 1.5s;\
+    animation-delay: 0.1s;\
+    animation-iteration-count: infinite;\
+    animation-timing-function: ease-out;\
+}\
+.bubbles02 {\
+    background-color: rgb(76, 0, 130);\
+    position: absolute;\
+    border-radius: 50%;\
+    animation-name: pop;\
+    animation-duration: 1.5s;\
+    animation-delay: 0.4s;\
+    animation-iteration-count: infinite;\
+    animation-timing-function: ease-out;\
+}\
+.bubbles03 {\
+    margin-left: 30px;\
+    background-color: rgb(76, 0, 130);\
+    position: absolute;\
+    border-radius: 50%;\
+    animation-name: pop;\
+    animation-duration: 1.5s;\
+    animation-delay: 0.6s;\
+    animation-iteration-count: infinite;\
+    animation-timing-function: ease-out;\
+}";
+
+var styleBubble = document.createElement('style');
+styleBubble.type = 'text/css';
+styleBubble.innerHTML = busyRoundedStyle;
+document.getElementsByTagName('head')[0].appendChild(styleBubble);
 
 //<i class="fas fa-circle-notch"></i>
 //HTML OBJECTS
@@ -83,7 +173,23 @@ var busyContainer = document.createElement("div");
 busyContainer.appendChild(icon);
 
 busyContainer.id = "busy";
-busyContainer.style = busyStyle;
+busyContainer.classList.add("busy");
+
+var busyContainerBubble = document.createElement("div");
+busyContainerBubble.id = "bubble";
+busyContainerBubble.classList.add("busy");
+
+var bubbles01 = document.createElement("div");
+bubbles01.classList.add("bubbles01");
+busyContainerBubble.appendChild(bubbles01);
+
+var bubbles02 = document.createElement("div");
+bubbles02.classList.add("bubbles02");
+busyContainerBubble.appendChild(bubbles02);
+
+var bubbles03 = document.createElement("div");
+bubbles03.classList.add("bubbles03");
+busyContainerBubble.appendChild(bubbles03);
 
 //MASTER OBJECT
 var busyIndicator = {
